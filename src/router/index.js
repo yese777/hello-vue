53f98dff90b5ffdeb24d14224e3370549bd6633f
yese <1321810135@qq.com>
2020-01-04 12:54:57 +0800

    初始化

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Login from "../views/Login"
+import Main from '../views/Main'
+
+// 用于嵌套的路由组件
+import UserProfile from '../views/user/Profile'
+import UserList from '../views/user/List'
+
+import NotFound from '../views/NotFound'
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      // 登录页
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      // 首页
+      path: '/main/:name',
+      props: true,
+      name: 'Main',
+      component: Main,
+      // 配置嵌套路由
+      children: [
+        {path: '/user/profile/:id', name: 'UserProfile', component: UserProfile, props: true},
+        {path: '/user/list', component: UserList},
+      ]
+    },
+    {
+      path: '/goHome',
+      redirect: '/main'
+    },
+    {
+      path: '*',
+      component: NotFound
+    }
+  ]
+});
